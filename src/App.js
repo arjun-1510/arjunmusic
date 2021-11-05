7e5b6ebcbce3fb71d75f0a6151d16a6a5ee08866
arjun-1510 <Arjung1510@gmail.com>
2021-11-05 20:32:51 +0530

    deploy react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "HBD Bhuvana",
+      artist: "college Beauty",
+      img_src: "./images/Bhuvana.jpeg",
+      src: "./music/UnnaiKandane.mp3",
+        
+    },
+    {
+      title: "Master",
+      artist: "Partz Grimbad",
+      img_src: "./images/Bhuvana.jpeg",
+      src: "./music/Master.mp3",
+        
+    },
+    {
+      title: "Kanavellam",
+      artist: "Hardy Sandhu",
+      img_src: "./images/titaliya.jpg",
+      src: "./music/Kanavellam.mp3",
+    },
+    {
+      title: "Anbe en Anbe",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/nach-meri-rani.jpg",
+      src: "./music/Anbe.mp3",
+    },
+    {
+      title: "Care Ni Karda",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/care-ni-karda.jpg",
+      src: "./music/Care Ni Karda.mp3",
+    },
+    {
+      title: "Burj Khalifa",
+      artist: "Shashi",
+      img_src: "./images/burjkalifa.jpg",
+      src: "./music/BurjKhalifa.mp3",
+    },
+    {
+      title: "Tango Del Fuego",
+      artist: "Parov Stelar",
+      img_src: "./images/ParovStelarGeorgiaGibbs-TangoDelFuego.jpg",
+      src: "./music/ParovStelarGeorgiaGibbs-TangoDelFuego.mp3",
+    },
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "./images/JasonDerulo-TakeYouDancing.jpg",
+      src: "./music/JasonDerulo-TakeYouDancing.mp3",
+    },
+    {
+      title: "Daisy",
+      artist: "Ashnikko",
+      img_src: "./images/Ashnikko-Daisy.jpg",
+      src: "./music/Ashnikko-Daisy.mp3",
+    },
+    {
+      title: "Dolly Song",
+      artist: "Partz Grimbad",
+      img_src: "./images/PatzGrimbard-DollySong.jpg",
+      src: "./music/PatzGrimbard-DollySong.mp3",
+    },
+    {
+      title: "Para Sundari",
+      artist: "Partz Grimbad",
+      img_src: "./images/PatzGrimbard-DollySong.jpg",
+      src: "./music/Param Sundari.mp3",
+    },
+    {
+      title: "HBD Bhuvana",
+      artist: "Partz Grimbad",
+      img_src: "./images/Bhuvana.jpeg",
+      src: "./music/UnnaiKandane.mp3",
+        
+    },
+
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
